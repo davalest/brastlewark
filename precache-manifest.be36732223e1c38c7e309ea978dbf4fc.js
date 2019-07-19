87efb077150520986fdf38372f5a454aab44005e
David Valenciano <dartvales@gmail.com>
2019-07-19 18:52:53 +0200

    Updates

---
+++ precache-manifest.be36732223e1c38c7e309ea978dbf4fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc76584258911f690d69753b4a733621",
+    "url": "/brastlewark/index.html"
+  },
+  {
+    "revision": "948ab573096b528bdb28",
+    "url": "/brastlewark/static/css/main.14c28c7a.chunk.css"
+  },
+  {
+    "revision": "5d41c8a2b01334c24975",
+    "url": "/brastlewark/static/js/2.6d809d18.chunk.js"
+  },
+  {
+    "revision": "948ab573096b528bdb28",
+    "url": "/brastlewark/static/js/main.c4d3c382.chunk.js"
+  },
+  {
+    "revision": "5a7f00ae99efbd12bd6e",
+    "url": "/brastlewark/static/js/runtime~main.c85b8f1b.js"
+  }
+]);
